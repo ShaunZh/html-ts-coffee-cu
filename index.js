0cea944bf31a23df6e3a5c7d6287655c95b5e529
zhangjie <beimian.zj@raycloud.com>
2025-06-03 19:18:18 +0800

    7. Sending contract transactions setup

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import { createWalletClient, custom } from 'https://esm.sh/viem'
+import { createWalletClient, custom, createPublicClient } from 'https://esm.sh/viem'
 
 const connectButton = document.getElementById('connectButton');
+const fundButton = document.getElementById('fundButton');
+const ethAmountInput = document.getElementById('ethAmount');
 
 let walletClient;
+let publicClient;
 
 async function connect() {
     if (typeof window.ethereum !== 'undefined') {
@@ -17,4 +20,31 @@ async function connect() {
     }
 }
 
+async function fund() {
+    const ethAmount = ethAmountInput.value;
+    if (typeof window.ethereum !== 'undefined') {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        })
+        await walletClient.requestAddresses()
+
+        publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        })
+
+        await publicClient.simulateContract({
+            // address: ???
+        })
+
+
+        console.log('hi')
+        connectButton.innerHTML = "Connected";
+    } else {
+        connectButton.innerHTML = 'Please install MetaMask!'
+    }
+
+    console.log(`Funding with ${ethAmount} ETH...`);
+}
+
 connectButton.onclick = connect;
+fundButton.onclick = fund;
